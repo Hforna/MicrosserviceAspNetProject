d234a667322eaabadeceb09cbd0c924e3820a2e8
Hforna <henriqueflashpingo@gmail.com>
2024-12-29 21:12:12 -0300

    First commit

---
+++ Catalog/Catalog.Api/Data/CatalogDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions dbContext) : base(dbContext)
+        {
+        }
+    }
+}
